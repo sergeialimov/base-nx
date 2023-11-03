2755394ca2cfc6bfe84e04280e114011dab67598
Sergei <dev.sergeialimov@gmail.com>
2023-11-03 22:12:24 +0400

    add watch mode

--- apps/base-nx-2/src/app/app.service.spec.ts
+++ apps/base-nx-2/src/app/app.service.spec.ts
@@ -14,8 +14,8 @@ describe('AppService', () => {
   });
 
   describe('getData', () => {
-    it('should return "Hello API"', () => {
-      expect(service.getData()).toEqual({ message: 'Hello API' });
+    it('should return "Hello Nest API"', () => {
+      expect(service.getData()).toEqual({ message: 'Hello Nest API' });
     });
   });
 });
